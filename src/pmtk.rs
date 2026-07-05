dc6d651d183f60d591eb1dcd115de42285bed2c0
Jonathan D. Baker <jonathan@ardent.tech>
2026-07-05 09:39:03 -0600

    add 220 cmd

--- src/pmtk.rs
+++ src/pmtk.rs
@@ -1,26 +1,47 @@
 use core::fmt::{Display, Formatter};
 use heapless::{format, String};
 
-const PACKET_LEN: usize = 255;
+const SET_NMEA_UPDATE_RATE: u16 = 220;
+const SET_NMEA_OUTPUT: u16 = 314;
 
-fn generate_checksum(data: &[u8]) -> [u8; 2] {
-    let mut checksum = 0;
-    for c in data {
-        checksum ^= *c;
+struct NmeaPacket<const N: usize> {
+    pmtk_command: PmtkCommand<N>
+}
+impl<const N: usize> Display for NmeaPacket<N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        // TODO can capacity be calculated dynamically? just set to 255?
+        let cmd: String<51> = format!("{}", self.pmtk_command)?;
+        let checksum = generate_checksum(cmd.as_bytes());
+        write!(f, "${}*{}{}\r\n", cmd, checksum[0] as char, checksum[1] as char)
     }
-    let msg = format!(2; "{:X?}", checksum).unwrap();
-    msg.into_bytes().into_array().unwrap()
 }
 
+struct PmtkCommand<const N: usize> {
+    data_fields: [u16; N],
+    pkt_type: u16
+}
+impl<const N: usize> Display for PmtkCommand<N> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        write!(f, "PMTK{}", self.pkt_type)?;
+        for c in self.data_fields {
+            write!(f, ",{}", c)?;
+        }
+        Ok(())
+    }
+}
+
+// "PMTK220,1000"
 #[derive(Debug, Default)]
-enum FrequencySetting {
-    #[default]
-    Disabled = 0x0,
-    OnePositionFix = 0x1,
-    TwoPositionFix = 0x2,
-    ThreePositionFix = 0x3,
-    FourPositionFix = 0x4,
-    FivePositionFix = 0x5,
+pub struct SetNmeaUpdateRate {
+    ms: u16 // TODO between 100 and 10000
+}
+impl Into<PmtkCommand<1>> for SetNmeaUpdateRate {
+    fn into(self) -> PmtkCommand<1> {
+        PmtkCommand {
+            data_fields: [self.ms],
+            pkt_type: SET_NMEA_UPDATE_RATE
+        }
+    }
 }
 
 // TODO builder pattern?
@@ -35,71 +56,43 @@ pub struct SetNmeaOutput {
     zda: FrequencySetting,
     mchn: FrequencySetting,
 }
-
-impl Into<DataField> for SetNmeaOutput {
-    fn into(self) -> DataField {
-        DataField([
-            self.gll as u8,
-            self.rmc as u8,
-            self.vtg as u8,
-            self.gga as u8,
-            self.gsa as u8,
-            self.gsv as u8,
-            0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-            self.zda as u8,
-            self.mchn as u8,
-        ])
-    }
-}
-
-// TODO define supertrait for Into
-impl Into<Command> for SetNmeaOutput {
-    fn into(self) -> Command {
-        // TODO figure out where to put PktType
-        Command::new(self.into(), PktType([0x33, 0x31, 0x34]))
-    }
-}
-
-#[derive(Debug, Default)]
-struct DataField([u8; 19]);
-impl Display for DataField {
-    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        for c in self.0 {
-            write!(f, ",{}", c)?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Debug, Default)]
-struct PktType([u8; 3]);
-impl Display for PktType {
-    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{}{}{}", self.0[0] as char, self.0[1] as char, self.0[2] as char)
-    }
-}
-
-#[derive(Debug, Default)]
-struct Command {
-    data_field: DataField,
-    pkt_type: PktType
-}
-
-impl Command {
-    fn new(data_field: DataField, pkt_type: PktType) -> Self {
-        Self {
-            data_field,
-            pkt_type,
+impl Into<PmtkCommand<19>> for SetNmeaOutput {
+    fn into(self) -> PmtkCommand<19> {
+        PmtkCommand {
+            data_fields: [
+                self.gll as u16,
+                self.rmc as u16,
+                self.vtg as u16,
+                self.gga as u16,
+                self.gsa as u16,
+                self.gsv as u16,
+                0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+                self.zda as u16,
+                self.mchn as u16,
+            ],
+            pkt_type: SET_NMEA_OUTPUT
         }
     }
 }
 
-impl Display for Command {
-    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        let payload: String<45> = format!("PMTK{}{}", self.pkt_type, self.data_field)?;
-        let checksum = generate_checksum(payload.as_bytes());
-        write!(f, "$PMTK{}{}*{}{}\r\n", self.pkt_type, self.data_field, checksum[0] as char, checksum[1] as char)
+fn generate_checksum(data: &[u8]) -> [u8; 2] {
+    let mut checksum = 0;
+    for c in data {
+        checksum ^= *c;
     }
+    let msg = format!(2; "{:X?}", checksum).unwrap();
+    msg.into_bytes().into_array().unwrap()
+}
+//
+#[derive(Debug, Default)]
+enum FrequencySetting {
+    #[default]
+    Disabled = 0x0,
+    OnePositionFix = 0x1,
+    TwoPositionFixes = 0x2,
+    ThreePositionFixes = 0x3,
+    FourPositionFixes = 0x4,
+    FivePositionFixes = 0x5,
 }
 
 #[cfg(test)]
@@ -107,40 +100,39 @@ mod tests {
     use super::*;
 
     #[test]
-    fn pkt_type_display_ok() {
-        let pkt_type = PktType([0x33, 0x31, 0x34]);
-        let res = format!(3; "{}", pkt_type).unwrap();
-        assert_eq!(res, "314");
-    }
-
-    #[test]
-    fn set_nmea_output_ok() {
-        let mut data = SetNmeaOutput::default();
-        data.gll = FrequencySetting::OnePositionFix;
-        let df: DataField = data.into();
-        assert_eq!(df.0, [0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0])
-    }
-
-    #[test]
-    fn data_field_display_ok() {
-        let mut data = SetNmeaOutput::default();
-        data.gll = FrequencySetting::OnePositionFix;
-        let df: DataField = data.into();
-        let res = format!(38; "{}", df).unwrap();
-        assert_eq!(res, ",1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
-    }
-
-    #[test]
-    fn command_display_ok() {
+    fn set_nmea_output_display_ok() {
         let mut data = SetNmeaOutput::default();
         data.gll = FrequencySetting::OnePositionFix;
         data.rmc = FrequencySetting::OnePositionFix;
         data.vtg = FrequencySetting::OnePositionFix;
         data.gga = FrequencySetting::OnePositionFix;
         data.gsa = FrequencySetting::OnePositionFix;
-        data.gsv = FrequencySetting::FivePositionFix;
-        let cmd: Command = data.into();
-        let res = format!(51; "{}", cmd).unwrap();
+        data.gsv = FrequencySetting::FivePositionFixes;
+        let pmtk_command: PmtkCommand<19> = data.into();
+        let res = format!(45; "{}", pmtk_command).unwrap();
+        assert_eq!(res, "PMTK314,1,1,1,1,1,5,0,0,0,0,0,0,0,0,0,0,0,0,0");
+    }
+
+    #[test]
+    fn set_nmea_update_rate_display_ok() {
+        let data = SetNmeaUpdateRate { ms: 1000 };
+        let pmtk_command: PmtkCommand<1> = data.into();
+        let res = format!(15; "{}", pmtk_command).unwrap();
+        assert_eq!(res, "PMTK220,1000");
+    }
+
+    #[test]
+    fn nmea_packet_display_ok() {
+        let mut data = SetNmeaOutput::default();
+        data.gll = FrequencySetting::OnePositionFix;
+        data.rmc = FrequencySetting::OnePositionFix;
+        data.vtg = FrequencySetting::OnePositionFix;
+        data.gga = FrequencySetting::OnePositionFix;
+        data.gsa = FrequencySetting::OnePositionFix;
+        data.gsv = FrequencySetting::FivePositionFixes;
+        let pmtk_command: PmtkCommand<19> = data.into();
+        let nmea_packet = NmeaPacket { pmtk_command };
+        let res = format!(51; "{}", nmea_packet).unwrap();
         assert_eq!(res, "$PMTK314,1,1,1,1,1,5,0,0,0,0,0,0,0,0,0,0,0,0,0*2C\r\n");
     }
 }
